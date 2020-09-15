90276864e5ecb308b023b5babf794737b2a1b8e1
= <brian.millerberg@gmail.com>
2020-09-14 18:59:27 -0600

    Added index file to repo

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Dating Website Index</title>
+    </head>
+    <body>
+        <center>
+            <h1>Dating Website Index</h1>
+        </center>
+    </body>
+</html>
