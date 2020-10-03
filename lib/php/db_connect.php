fd79ca23e86277334e8399bd344dfabd25b56a52
Brian Millerberg <brian.millerberg@gmail.com>
2020-10-03 14:38:12 -0600

    Separate JavaScript and PHP libraries into separate directories

---
+++ lib/php/db_connect.php
@@ -0,0 +1,35 @@
+<?php
+
+
+function getConnection(){
+	$mysqli = new mysqli('136.36.9.168', 'cs452', 'UTCLwQln%&zBfu7Ja5P8@s$KS', 'Dating_Website', 33306);
+	if ($mysqli->connect_errno){
+		echo "Failed to connect to database.";
+		die();
+	}
+	else {
+		return $mysqli;
+	}
+}
+
+function doSelect($query){
+	$returnArray = array();
+	
+	$conn = getConnection();
+	
+	$result = $conn->query($query);
+	
+	
+
+	if ($result) {
+	  // output data of each row
+	  while ($row = mysqli_fetch_assoc($result)){
+		array_push($returnArray, $row);
+	  }
+	}
+	
+	$conn->close();
+	
+	return $returnArray;
+}
+
