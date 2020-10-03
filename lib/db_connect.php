f463a5283f4b979d284b88ec2b8eb0b9b12d1911
= <brian.millerberg@gmail.com>
2020-10-02 20:03:49 -0600

    Added database connections

---
+++ lib/db_connect.php
@@ -0,0 +1,7 @@
+<?php
+$mysqli = new mysqli('136.36.9.168', 'cs452', 'UTCLwQln%&zBfu7Ja5P8@s$KS', 'Dating_Website', 33306);
+if ($mysqli->connect_errno){
+	echo "Failed to connect to database.";
+	die();
+}
+?>
